f3d8029a8b74e50ee56641fa0372ebf5c4395434
Adam Renaud <adamf.renaud@gmail.com>
2018-11-05 09:16:18 -0500

    Adding the basic framework for the app

---
+++ AutoList-Desktop/ViewModels/CommandBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AutoList_Desktop.ViewModels
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Executes an action
+    /// </summary>
+    public class CommandBase : ICommand
+    {
+        private readonly Action _action;
+
+        public CommandBase(Action action) { _action = action; }
+
+        public bool CanExecute(object parameter) { return true; }
+
+        public void Execute(object parameter) { _action(); }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ AutoList-Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using AutoList_Desktop.Annotations;
+
+namespace AutoList_Desktop.ViewModels
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// The ViewModel Base Object
+    /// </summary>
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        /// <inheritdoc />
+        /// <summary>
+        /// The Event the fires when a property changes
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// The invocation function for the <see cref="PropertyChanged"/> Event
+        /// </summary>
+        /// <param name="propertyName">The Property Name</param>
+        [NotifyPropertyChangedInvocator]
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
